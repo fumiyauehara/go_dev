fdcf773cb375860592f3d40c8c41fb5f78be6e51
fumiya-uehara <edenofeast515@outlook.jp>
2019-11-03 14:26:56 +0900

    update 記事投稿用に変更

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"net/http"
 	"github.com/gin-gonic/gin"
-	"github.com/fumiya-uehara/go_dev/database"
+	"net/http"
 )
 
 func main() {
@@ -19,16 +18,6 @@ func main() {
 		msg.Message = "これは構造体をJSONで返すためのテストです。"
 		msg.Number = 1111
 		context.JSON(http.StatusOK, msg)
-	})
-
-	r.GET("/get_user", func(context *gin.Context) {
-		result := database.User{}.FindUser(1)
-		context.JSON(http.StatusOK, result)
-	})
-
-	r.GET("/store_user", func(context *gin.Context) {
-		database.User{FirstName: "Test", LastName: "Taro", Age: 39}.StoreUser()
-		context.JSON(http.StatusCreated, "create is OK")
 	})
 
 	_ = r.Run(":9000")
